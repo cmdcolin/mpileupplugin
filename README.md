bf8b8705badb8e4e74281502bb2d2961e157aff7
Colin <colin.diesh@gmail.com>
2020-04-06 03:35:11 -0400

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# mpilupplugin
+
+
+## Intallation
+
+Clone the repository to the jbrowse plugins subdirectory and name it MPilup
+
+    git clone https://github.com/cmdcolin/mpilupplugin MPilup
+
+Then add the plugin to your configuration, e.g. "plugins": ["MPilup"]
+
+See http://gmod.org/wiki/JBrowse_FAQ#How_do_I_install_a_plugin for details
---
+++ js/Store/SeqFeature/MPilup.js
@@ -0,0 +1,33 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/array',
+    'JBrowse/Store/SeqFeature',
+    'JBrowse/Util',
+    'JBrowse/Model/CoverageFeature'
+],
+function (
+    declare,
+    array,
+    SeqFeatureStore,
+    Util,
+    CoverageFeature
+) {
+    return declare(SeqFeatureStore, {
+        constructor: function (args) {
+            this.store = args.store;
+        },
+
+        getGlobalStats: function (callback /* , errorCallback */) {
+            callback({});
+        },
+
+        getFeatures: function (query, featureCallback, finishCallback, errorCallback) {
+            this.store.getFeatures(query, function (res) {
+                finishCallback();
+            },
+            finishCallback,
+            errorCallback
+            );
+        }
+    });
+});
